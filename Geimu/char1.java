bcdfbd06a5634b1bcd626fbe1ba90d1b3ced29e0
DESKTOP-HGS4KPC\ad1 <rizkyadi245@gmail.com>
2018-04-24 16:18:20 +0800

    pertama

---
+++ Geimu/char1.java
@@ -0,0 +1,71 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class char1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class char1 extends Actor
+{
+    private int vSpeed=7;
+    private int speed=5;
+    private int acceleration=2;
+    /**
+     * Act - do whatever the char1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+
+    public void act() 
+    {
+        checkKeys();
+        checkFall();
+    }
+    
+    private void checkKeys()
+    {
+        if (Greenfoot.isKeyDown("left"))
+        {
+            setImage("char1.png");
+            moveLeft();
+        }
+        if (Greenfoot.isKeyDown("right"))
+        {
+            setImage("char1.png");
+            moveRight();
+        }
+    }
+    
+    public void checkFall()
+    {
+        if(ontanah())
+        {
+            speed=0;
+        }
+        else{
+            fall();
+        }
+    }
+    
+    public boolean ontanah()
+    {
+        Actor under=getOneObjectAtOffset (0,getImage().getHeight()/2, tanah.class);
+        return under!= null;
+    }
+    
+    public void fall()
+    {
+        setLocation(getX(), getY()+speed);
+        speed=speed+acceleration;
+    }
+    
+    public void moveRight()
+    {
+        setLocation(getX()+vSpeed, getY());
+    }
+    
+    public void moveLeft()
+    {
+        setLocation(getX()-vSpeed, getY());
+    }
+}
---
+++ Geimu/world.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class world extends World
+{
+    public char1 char1=new char1();
+    public tanah tanah=new tanah();
+    /**
+     * Constructor for objects of class world.
+     * 
+     */
+    public world()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        addObject(tanah,300,600);
+        addObject(char1,30,280);
+    }
+}
